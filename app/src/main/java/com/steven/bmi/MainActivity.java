5bbc4d33b8c865ace2c39f22d412cfaaf75a3f4f
kevienjun <h1253516666@gmail.com>
2018-10-09 16:26:40 +0800

    Bmi

--- app/src/main/java/com/steven/bmi/MainActivity.java
+++ app/src/main/java/com/steven/bmi/MainActivity.java
@@ -1,23 +1,31 @@
 package com.steven.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edWeight;
+    EditText edHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+
     }
 
     public void bmi(View view){
         Log.d("MainActivity" , "bmi");
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edHeight = findViewById(R.id.ed_height);
+        final EditText edWeight = findViewById(R.id.ed_weight);
+        final EditText edHeight = findViewById(R.id.ed_height);
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
         Log.d("MainActivity" , w + "/" + h);
@@ -25,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
         Log.d("MainActivity" , bmi +"");
+        Toast.makeText(this,"Your Bmi is" + bmi,
+                Toast.LENGTH_LONG).show();
+                //下方出現資料
+        new AlertDialog.Builder(this)
+                .setMessage("Your Bmi is" + bmi)
+                .setTitle("Bmi")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edWeight.setText("");
+                        //清除輸入的資料
+                    }
+                })
+                .show();
+                //彈出的視窗
     }
 }
